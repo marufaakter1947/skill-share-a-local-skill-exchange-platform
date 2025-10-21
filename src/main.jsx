e2d7af305072d1a5cfd7b21afd548d605d2f1f9c
Marufa Akter <marufaakter1947@gmail.com>
2025-10-21 22:45:28 +0600

    install react router,Tailwind css, daisy ui , Animate css,react-hot-toast , swiper and react icons.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from './Pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </StrictMode>,
 )
