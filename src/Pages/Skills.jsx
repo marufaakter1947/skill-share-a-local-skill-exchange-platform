6e5e29e337da0ce7451390fb40c0e1b74b592cd8
Marufa Akter <marufaakter1947@gmail.com>
2025-10-22 14:48:19 +0600

    All skills added to skills page

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+
 import useSkills from '../Hooks/useSkills';
+import SkillCard from '../Components/SkillCard';
+import { NavLink } from 'react-router';
 
 const Skills = () => {
-    const { skills, } = useSkills();
-    return (
-        <div>
-            {skills.length} skills loaded
-        </div>
-    );
+  const { skills } = useSkills();
+
+  return (
+    <div>
+      <h1 className="text-[48px] font-bold text-center mb-4  ">
+        All Skills
+      </h1>
+      <p className="text-xl text-[#627382] text-center mb-10">
+        Explore Every Skill — From Coding to Cooking!
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {skills.map((skill) => (
+          <SkillCard key={skill.id} skill={skill} />
+        ))}
+      </div>
+
+      
+    </div>
+  );
 };
 
 export default Skills;
