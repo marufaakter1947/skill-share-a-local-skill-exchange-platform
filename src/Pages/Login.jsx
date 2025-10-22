592f0bdc5b129e80eb7d3649b3b3ef333516ced2
Marufa Akter <marufaakter1947@gmail.com>
2025-10-22 23:11:21 +0600

    Added Google Signin with popup

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,14 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router";
 import { auth } from "../Firebase/Firebase.config";
 import toast from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 
+const googleProvider = new GoogleAuthProvider();
 const Login = () => {
-    
+    const [user,setUser] =useState(null);
   const [show, setShow] = useState(false);
 
   const handleSignin = (e) => {
@@ -17,7 +19,8 @@ const Login = () => {
 
 
     signInWithEmailAndPassword(auth,email,password)
-     .then(()=>{
+     .then((res)=>{
+        setUser(res.user);
             toast.success("Login Successful")
         })
         .catch((e)=>{
@@ -25,14 +28,43 @@ const Login = () => {
         })
   };
 
+  const handleGoogleSignin = ()=>{
+    signInWithPopup(auth,googleProvider)
+    .then(()=>{
+            toast.success("Signin Successful");
+        })
+        .catch((e)=>{
+            toast.error(e.message);
+        })
+  }
+
+  const handleSignout = ()=>{
+    signOut(auth)
+   .then(()=>{
+            toast.success("Signout Successful");
+            setUser(null);
+        })
+        .catch((e)=>{
+            toast.error(e.message);
+        })
+  }
+
   return (
     <div className="hero bg-base-200 min-h-screen  ">
       <div className="hero-content flex-col  ">
-        {/* <form onSubmit={handleSignin}> */}
+       {
+        user ? (
+            <div className="text-center space-y-3">
+                <img src={user?.photoURL || "https://via.placeholder.com/88"} className="h-20 w-20 rounded-full mx-auto" alt="" />
+             <h2 className="text-xl font-semibold">{user?.displayName}</h2>
+             <button onClick={handleSignout} className="btn btn-primary">SignOut</button>
+            </div>
+        ) : (
+             <form onSubmit={handleSignin}>
           <div className="card bg-base-100 w-full max-w-md md:max-w-lg shrink-0 shadow-2xl p-6">
             <h1 className="text-5xl font-bold text-center mb-4">Login now!</h1>
 
-           <form onSubmit={handleSignin}>
+           
              <div className="card-body flex justify-center">
               <fieldset className="space-y-4 w-full">
                 <label className="block text-sm font-medium mb-1">Email</label>
@@ -71,6 +103,14 @@ const Login = () => {
                     Login
                   </button>
                 </div>
+                <div className="flex justify-center">
+                  <button onClick={handleGoogleSignin} className="btn btn-neutral w-96 max-w-full mt-4">
+                    <FcGoogle /> Continue with Google
+                  </button>
+                </div>
+
+
+
                 <p className="font-semibold text-center pt-5">
                   Don't Have an account?{" "}
                   <Link className="text-secondary hover:underline" to="/signup">
@@ -79,9 +119,11 @@ const Login = () => {
                 </p>
               </fieldset>
             </div>
-           </form>
+           
           </div>
-        {/* </form> */}
+        </form>
+        )
+       }
       </div>
     </div>
   );
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,116 +1,150 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-import { Link } from 'react-router';
-import { auth } from '../Firebase/Firebase.config';
-import toast from 'react-hot-toast';
+import { Link } from "react-router";
+import { auth } from "../Firebase/Firebase.config";
+import toast from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 
+const googleProvider = new GoogleAuthProvider();
 const Signup = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
+  const handleSignup = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // console.log("Sign up function entered",{email,password});
 
-   
-
-    const handleSignup =(e)=>{
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        // console.log("Sign up function entered",{email,password});
-
-         if(password.length<6){
-        toast.error("Length must be at least 6 character");
-        return;
+    if (password.length < 6) {
+      toast.error("Length must be at least 6 character");
+      return;
     }
 
     if (!/(?=.*[A-Z])/.test(password)) {
-  toast.error("Password must include at least one uppercase letter");
-  return;
-}
+      toast.error("Password must include at least one uppercase letter");
+      return;
+    }
     if (!/(?=.*[a-z])/.test(password)) {
-  toast.error("Must have a Lowercase letter in the password");
-  return;
-}
-
-        createUserWithEmailAndPassword(auth,email,password)
-        .then(()=>{
-            toast.success("Signup Successful")
-        })
-        .catch((e)=>{
-            toast.error(e.message);
-        })
+      toast.error("Must have a Lowercase letter in the password");
+      return;
     }
 
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        toast.success("Signup Successful");
+      })
+      .catch((e) => {
+        toast.error(e.message);
+      });
+  };
 
-    return (
-        <div className="hero bg-base-200 min-h-screen  ">
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(() => {
+        toast.success("Signin Successful");
+      })
+      .catch((e) => {
+        toast.error(e.message);
+      });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen  ">
       <div className="hero-content flex-col  ">
         <div className="card bg-base-100 w-full max-w-md md:max-w-lg shrink-0 shadow-2xl p-6">
-          <h1 className="text-5xl font-bold text-center mb-4">Join Us Today!</h1>
+          <h1 className="text-5xl font-bold text-center mb-4">
+            Join Us Today!
+          </h1>
           <form onSubmit={handleSignup}>
             <div className="card-body flex justify-center">
-            <fieldset className="space-y-4 w-full">
+              <fieldset className="space-y-4 w-full">
                 <label className="block text-sm font-medium mb-1">Name</label>
-                
-              <div className="flex justify-center">
-                
-                <input
-                  type="text"
-                  name='name'
-                  className="input input-bordered w-96 max-w-full "
-                  placeholder="Enter your Name" required
-                />
-              </div>
+
+                <div className="flex justify-center">
+                  <input
+                    type="text"
+                    name="name"
+                    className="input input-bordered w-96 max-w-full "
+                    placeholder="Enter your Name"
+                    required
+                  />
+                </div>
                 <label className="block text-sm font-medium mb-1">Email</label>
 
-              <div className="flex justify-center">
-                <input
-                  type="email"
-                  name='email'
-                  className="input input-bordered w-96 max-w-full "
-                  placeholder="Enter your Email" required
-                />
-              </div>
-                <label className="block text-sm font-medium mb-1">Photo-URL</label>
+                <div className="flex justify-center">
+                  <input
+                    type="email"
+                    name="email"
+                    className="input input-bordered w-96 max-w-full "
+                    placeholder="Enter your Email"
+                    required
+                  />
+                </div>
+                <label className="block text-sm font-medium mb-1">
+                  Photo-URL
+                </label>
 
-              <div className="flex justify-center">
-                <input
-                  type="text"
-                   name='photo'
-                  className="input input-bordered w-96 max-w-full "
-                  placeholder="Enter your Photo-URL" required
-                />
-              </div>
-                <label className="block text-sm font-medium mb-1">Password</label>
+                <div className="flex justify-center">
+                  <input
+                    type="text"
+                    name="photo"
+                    className="input input-bordered w-96 max-w-full "
+                    placeholder="Enter your Photo-URL"
+                    required
+                  />
+                </div>
+                <label className="block text-sm font-medium mb-1">
+                  Password
+                </label>
 
-              <div className="flex justify-center relative">
-                <input
-                  type={show ? "text" : "password"}
-                  name="password"
-                  className="input input-bordered w-96 max-w-full "
-                  placeholder="Enter your Password" required
-                />
-                 <span
-                  onClick={() => setShow(!show)}
-                  className="absolute right-2 top-[14px] cursor-pointer z-50"
-                >
-                  {show ? <FaEye /> : <FaEyeSlash />}
-                </span>
-              </div>
-              
-              <div className="flex justify-center">
-                <button className="btn btn-neutral w-96 max-w-full mt-4">
-                  Register
-                </button>
-              </div>
-               <p className='font-semibold text-center pt-5'>Already Have an account? <Link className='text-secondary hover:underline' to="/login">Login</Link></p>
-            </fieldset>
-          </div>
+                <div className="flex justify-center relative">
+                  <input
+                    type={show ? "text" : "password"}
+                    name="password"
+                    className="input input-bordered w-96 max-w-full "
+                    placeholder="Enter your Password"
+                    required
+                  />
+                  <span
+                    onClick={() => setShow(!show)}
+                    className="absolute right-2 top-[14px] cursor-pointer z-50"
+                  >
+                    {show ? <FaEye /> : <FaEyeSlash />}
+                  </span>
+                </div>
+
+                <div className="flex justify-center">
+                  <button className="btn btn-neutral w-96 max-w-full mt-4">
+                    Register
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button
+                    onClick={handleGoogleSignin}
+                    className="btn btn-neutral w-96 max-w-full mt-4"
+                  >
+                    <FcGoogle /> Continue with Google
+                  </button>
+                </div>
+                <p className="font-semibold text-center pt-5">
+                  Already Have an account?{" "}
+                  <Link className="text-secondary hover:underline" to="/login">
+                    Login
+                  </Link>
+                </p>
+              </fieldset>
+            </div>
           </form>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Signup;
